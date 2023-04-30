2fdc5a4eec32f15a231e373ce01bd7f094cd8520
paulie-of-punskas <pbernecki@protonmail.com>
2023-04-30 18:00:03 +0200

    Revert "Update server.py"
    This reverts commit f6cf6b8453eddb96910714c430cce8c33d7fa654.

--- final_project/server.py
+++ final_project/server.py
@@ -5,7 +5,7 @@ import json
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
+def englishToSpanish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
     return "Translated text to French"
